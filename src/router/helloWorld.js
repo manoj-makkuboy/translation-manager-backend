35cd924d74e075481fd5744e3a4a5851f1f580ef
Manoj Mohan <jesuismanoj@gmail.com>
2020-12-19 13:17:56 +0530

    Change api response schema

--- src/router/helloWorld.js
+++ src/router/helloWorld.js
@@ -2,9 +2,13 @@ const Koa = require("koa");
 const router = require("koa-router")();
 const bodyParser = require("koa-bodyparser");
 const app = new Koa();
+const cors = require('@koa/cors');
+
+let translations = require("../translations")
 
 app.use(bodyParser());
 app.use(router.routes()); // route middleware
+app.use(cors())
 // a simple car object that we can serve
 
 
@@ -23,21 +27,32 @@ router.post("/helloWorld", async (ctx, next) => {
 
 
 router.get("/getLanguages", async (ctx, next) => {
-  ctx.response.body = [
-    {languageId: "english", displayText: "English"},
-    {languageId: "Hindi", displayText: "Hindi"},
-    {languageId: "French", displayText: "French"},
-  ]
+  ctx.response.body = Object.keys(translations)
+  ctx.response.headers['Access-Control-Allow-Origin'] = "*"
   await next();
 });
 
 router.get("/getTranslation/:language", async (ctx, next) => {
-  let queryParams = ctx.params
-  console.log("queryParams", queryParams)
-  ctx.response.body = {
-    "HELLO_WORLD": "hello world",
-    "CANCEL": "cancel"
-  }
+  let requestLanguage = ctx.params.language
+  // console.log("translations ", translations)
+  ctx.response.body = translations[requestLanguage]
+  await next();
+});
+
+router.put("/translation", async (ctx, next) => {
+  console.log('payload in POST ', ctx.request.body)
+
+  console.log("original Translations",translations )
+  translations = ctx.request.body
+  console.log("new Translations",translations )
+
+  ctx.response.body = translations;
+  await next();
+});
+
+router.get("/getTranslation/", async (ctx, next) => {
+  // console.log("translations ", translations)
+  ctx.response.body = translations
   await next();
 });
 
